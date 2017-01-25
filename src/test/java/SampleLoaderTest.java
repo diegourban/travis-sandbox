dd1f3499b25622b9fd4d00647b656de6bcb92526
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-25 09:34:31 -0200

    Add files via upload

--- src/test/java/SampleLoaderTest.java
+++ src/test/java/SampleLoaderTest.java
@@ -9,11 +9,12 @@ public class SampleLoaderTest {
     @Test
     public void shouldLoadInputFileContentAsString() throws IOException {
         StringBuilder sb = new StringBuilder()
-                .append(1)
+                .append("line 1")
                 .append(System.lineSeparator())
-                .append(2)
+                .append("line 2")
                 .append(System.lineSeparator())
-                .append(3);
+                .append("line 3")
+                .append(System.lineSeparator());
         String expectedContent = sb.toString();
 
         SampleLoader sl = new SampleLoader();
@@ -22,33 +23,4 @@ public class SampleLoaderTest {
         assertEquals(expectedContent, content);
     }
 
-    @Test
-    public void shouldLoadOutputFileContentAsString() throws IOException {
-        StringBuilder sb = new StringBuilder()
-                .append(6)
-                .append(System.lineSeparator());
-        String expectedContent = sb.toString();
-
-        SampleLoader sl = new SampleLoader();
-        String content = sl.loadAsString("sample.out");
-
-        assertEquals(expectedContent, content);
-    }
-
-    @Test
-    public void shouldLoadLowestInputFileContentAsString() throws IOException {
-        StringBuilder sb = new StringBuilder()
-                .append("132")
-                .append(System.lineSeparator())
-                .append("323")
-                .append(System.lineSeparator())
-                .append("231")
-                .append(System.lineSeparator());
-        String expectedContent = sb.toString();
-
-        SampleLoader sl = new SampleLoader();
-        String content = sl.loadAsString("lowestInput.out");
-
-        assertEquals(expectedContent, content);
-    }
 }
